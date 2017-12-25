85cdfc86ca3382f475c2ed1594fccd1bbe9d264e
XxCerebratexX <macbethrash@gmail.com>
2017-12-25 15:58:56 -0700

    2

--- Lista/Lista/Form1.Designer.cs
+++ Lista/Lista/Form1.Designer.cs
@@ -33,18 +33,20 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.Lista = new System.Windows.Forms.ListBox();
-            this.videosLista = new System.Windows.Forms.ListBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.EliminarLista = new System.Windows.Forms.Button();
-            this.ModificarLista = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.Subir = new System.Windows.Forms.Button();
-            this.Bajar = new System.Windows.Forms.Button();
             this.Quitar = new System.Windows.Forms.Button();
+            this.Bajar = new System.Windows.Forms.Button();
+            this.Subir = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ModificarLista = new System.Windows.Forms.Button();
+            this.EliminarLista = new System.Windows.Forms.Button();
+            this.videosLista = new System.Windows.Forms.ListBox();
+            this.Lista = new System.Windows.Forms.ListBox();
+            this.nuevaLista = new System.Windows.Forms.Button();
+            this.nuevoVideo = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -98,42 +100,56 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Listas";
             // 
-            // Lista
+            // panel2
             // 
-            this.Lista.FormattingEnabled = true;
-            this.Lista.ItemHeight = 20;
-            this.Lista.Location = new System.Drawing.Point(7, 29);
-            this.Lista.Name = "Lista";
-            this.Lista.Size = new System.Drawing.Size(146, 144);
-            this.Lista.TabIndex = 0;
+            this.panel2.Controls.Add(this.nuevoVideo);
+            this.panel2.Controls.Add(this.Quitar);
+            this.panel2.Controls.Add(this.Bajar);
+            this.panel2.Controls.Add(this.Subir);
+            this.panel2.Location = new System.Drawing.Point(225, 196);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(100, 143);
+            this.panel2.TabIndex = 3;
             // 
-            // videosLista
+            // Quitar
             // 
-            this.videosLista.FormattingEnabled = true;
-            this.videosLista.ItemHeight = 20;
-            this.videosLista.Location = new System.Drawing.Point(13, 196);
-            this.videosLista.Name = "videosLista";
-            this.videosLista.Size = new System.Drawing.Size(206, 144);
-            this.videosLista.TabIndex = 1;
+            this.Quitar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Quitar.Location = new System.Drawing.Point(13, 109);
+            this.Quitar.Name = "Quitar";
+            this.Quitar.Size = new System.Drawing.Size(75, 23);
+            this.Quitar.TabIndex = 2;
+            this.Quitar.Text = "Quitar";
+            this.Quitar.UseVisualStyleBackColor = true;
+            // 
+            // Bajar
+            // 
+            this.Bajar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bajar.Location = new System.Drawing.Point(13, 80);
+            this.Bajar.Name = "Bajar";
+            this.Bajar.Size = new System.Drawing.Size(75, 23);
+            this.Bajar.TabIndex = 1;
+            this.Bajar.Text = "Bajar";
+            this.Bajar.UseVisualStyleBackColor = true;
+            // 
+            // Subir
+            // 
+            this.Subir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Subir.Location = new System.Drawing.Point(13, 51);
+            this.Subir.Name = "Subir";
+            this.Subir.Size = new System.Drawing.Size(75, 23);
+            this.Subir.TabIndex = 0;
+            this.Subir.Text = "Subir";
+            this.Subir.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.nuevaLista);
             this.panel1.Controls.Add(this.ModificarLista);
             this.panel1.Controls.Add(this.EliminarLista);
             this.panel1.Location = new System.Drawing.Point(162, 29);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(99, 143);
             this.panel1.TabIndex = 2;
-            // 
-            // EliminarLista
-            // 
-            this.EliminarLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EliminarLista.Location = new System.Drawing.Point(12, 43);
-            this.EliminarLista.Name = "EliminarLista";
-            this.EliminarLista.Size = new System.Drawing.Size(75, 23);
-            this.EliminarLista.TabIndex = 0;
-            this.EliminarLista.Text = "Eliminar";
-            this.EliminarLista.UseVisualStyleBackColor = true;
             // 
             // ModificarLista
             // 
@@ -145,45 +161,54 @@
             this.ModificarLista.Text = "Modificar";
             this.ModificarLista.UseVisualStyleBackColor = true;
             // 
-            // panel2
+            // EliminarLista
             // 
-            this.panel2.Controls.Add(this.Quitar);
-            this.panel2.Controls.Add(this.Bajar);
-            this.panel2.Controls.Add(this.Subir);
-            this.panel2.Location = new System.Drawing.Point(225, 196);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(100, 143);
-            this.panel2.TabIndex = 3;
+            this.EliminarLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EliminarLista.Location = new System.Drawing.Point(12, 43);
+            this.EliminarLista.Name = "EliminarLista";
+            this.EliminarLista.Size = new System.Drawing.Size(75, 23);
+            this.EliminarLista.TabIndex = 0;
+            this.EliminarLista.Text = "Eliminar";
+            this.EliminarLista.UseVisualStyleBackColor = true;
             // 
-            // Subir
+            // videosLista
             // 
-            this.Subir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Subir.Location = new System.Drawing.Point(13, 28);
-            this.Subir.Name = "Subir";
-            this.Subir.Size = new System.Drawing.Size(75, 23);
-            this.Subir.TabIndex = 0;
-            this.Subir.Text = "Subir";
-            this.Subir.UseVisualStyleBackColor = true;
+            this.videosLista.FormattingEnabled = true;
+            this.videosLista.ItemHeight = 20;
+            this.videosLista.Location = new System.Drawing.Point(13, 196);
+            this.videosLista.Name = "videosLista";
+            this.videosLista.Size = new System.Drawing.Size(206, 144);
+            this.videosLista.TabIndex = 1;
             // 
-            // Bajar
+            // Lista
             // 
-            this.Bajar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Bajar.Location = new System.Drawing.Point(13, 57);
-            this.Bajar.Name = "Bajar";
-            this.Bajar.Size = new System.Drawing.Size(75, 23);
-            this.Bajar.TabIndex = 1;
-            this.Bajar.Text = "Bajar";
-            this.Bajar.UseVisualStyleBackColor = true;
+            this.Lista.FormattingEnabled = true;
+            this.Lista.ItemHeight = 20;
+            this.Lista.Location = new System.Drawing.Point(7, 29);
+            this.Lista.Name = "Lista";
+            this.Lista.Size = new System.Drawing.Size(146, 144);
+            this.Lista.TabIndex = 0;
             // 
-            // Quitar
+            // nuevaLista
             // 
-            this.Quitar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Quitar.Location = new System.Drawing.Point(13, 86);
-            this.Quitar.Name = "Quitar";
-            this.Quitar.Size = new System.Drawing.Size(75, 23);
-            this.Quitar.TabIndex = 2;
-            this.Quitar.Text = "Quitar";
-            this.Quitar.UseVisualStyleBackColor = true;
+            this.nuevaLista.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.nuevaLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nuevaLista.Location = new System.Drawing.Point(38, 13);
+            this.nuevaLista.Name = "nuevaLista";
+            this.nuevaLista.Size = new System.Drawing.Size(24, 24);
+            this.nuevaLista.TabIndex = 2;
+            this.nuevaLista.Text = "+";
+            this.nuevaLista.UseVisualStyleBackColor = true;
+            // 
+            // nuevoVideo
+            // 
+            this.nuevoVideo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nuevoVideo.Location = new System.Drawing.Point(34, 22);
+            this.nuevoVideo.Name = "nuevoVideo";
+            this.nuevoVideo.Size = new System.Drawing.Size(23, 23);
+            this.nuevoVideo.TabIndex = 3;
+            this.nuevoVideo.Text = "+";
+            this.nuevoVideo.UseVisualStyleBackColor = true;
             // 
             // listasVentana
             // 
@@ -198,8 +223,8 @@
             this.Name = "listasVentana";
             this.Text = "Listas";
             this.groupBox1.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -220,6 +245,8 @@
         private System.Windows.Forms.Button EliminarLista;
         private System.Windows.Forms.ListBox videosLista;
         private System.Windows.Forms.ListBox Lista;
+        private System.Windows.Forms.Button nuevoVideo;
+        private System.Windows.Forms.Button nuevaLista;
     }
 }
 
---
+++ Lista/Lista/nuevaLista.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Lista
+{
+    partial class nuevaLista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nombreLista = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.guardar = new System.Windows.Forms.Button();
+            this.cancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // nombreLista
+            // 
+            this.nombreLista.Location = new System.Drawing.Point(65, 22);
+            this.nombreLista.Name = "nombreLista";
+            this.nombreLista.Size = new System.Drawing.Size(226, 20);
+            this.nombreLista.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "¿Para que días le gustaría usar esta lista?";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Todos los días",
+            "Lunes",
+            "Martes",
+            "Miercoles",
+            "Jueves",
+            "Viernes",
+            "Sabado",
+            "Domingo"});
+            this.checkedListBox1.Location = new System.Drawing.Point(15, 99);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 124);
+            this.checkedListBox1.TabIndex = 3;
+            // 
+            // guardar
+            // 
+            this.guardar.Location = new System.Drawing.Point(44, 255);
+            this.guardar.Name = "guardar";
+            this.guardar.Size = new System.Drawing.Size(75, 23);
+            this.guardar.TabIndex = 4;
+            this.guardar.Text = "Guardar";
+            this.guardar.UseVisualStyleBackColor = true;
+            // 
+            // cancelar
+            // 
+            this.cancelar.Location = new System.Drawing.Point(171, 255);
+            this.cancelar.Name = "cancelar";
+            this.cancelar.Size = new System.Drawing.Size(75, 23);
+            this.cancelar.TabIndex = 5;
+            this.cancelar.Text = "Cancelar";
+            this.cancelar.UseVisualStyleBackColor = true;
+            // 
+            // nuevaLista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 326);
+            this.Controls.Add(this.cancelar);
+            this.Controls.Add(this.guardar);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nombreLista);
+            this.Controls.Add(this.label1);
+            this.Name = "nuevaLista";
+            this.Text = "Nueva Lista";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nombreLista;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button guardar;
+        private System.Windows.Forms.Button cancelar;
+    }
+}
